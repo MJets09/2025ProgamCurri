801d9d02253dfc722038bf94d07d2252b7e3d4a2
Meng <mthaoqk@gmail.com>
2025-02-10 20:58:07 -0600

    2.10.25

---
+++ reactTuturial/reactTest/src/App.tsx
@@ -0,0 +1,9 @@
+//App.tsx is your main page?
+
+import Message from './Message'
+
+function App() {
+  return <div><Message></Message></div>
+}
+
+export default App;
---
+++ reactTuturial/reactTest/src/Message.tsx
@@ -0,0 +1,14 @@
+
+//Creating a new function component
+//Always PascalCasing
+function Message() {
+    //JSX: Javascript XML
+    //Allows to creat dynamic reactions
+    const name = '';
+    //Expression a piece of code that returns a vaule
+   return <h1>{name ? `The World is Yours ${name}` : 'Sup'}</h1>
+}
+
+export default Message;
+
+//Left off at 16:40
