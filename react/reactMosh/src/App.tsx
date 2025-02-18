1ea054e838e7a096cb62fc35be8927a0e1cfa1fa
MJets09 <mthaoqk@gmail.com>
2025-02-17 20:32:52 -0600

    2.17.25

--- react/reactMosh/src/App.tsx
+++ react/reactMosh/src/App.tsx
@@ -1,9 +1,15 @@
 import ListGroup from "./components/listGroup";
+let cities = ["Saint Paul", "Minneapolis", "Seattle"];
+const onSelectItem = (item: string) => {
+  console.log(item)
+}
 
 function App() {
+  
   return (
+    
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup items={cities} heading="Cities" onSelectItem={onSelectItem}></ListGroup>
     </div>
   );
 }
--- react/reactMosh/src/components/listGroup.tsx
+++ react/reactMosh/src/components/listGroup.tsx
@@ -1,20 +1,34 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
-function ListGroup() {
-  let items = ["Saint Paul", "Minneapolis", "Seattle"];
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   // items = [];
-
-  
-  const handleClick = (event: MouseEvent) => console.log(event.target);
+  //Each component has its own useState
+  const [selectIndex, SetIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items list</p>}
       <ul className="list-group">
-        {items.map((items) => (
-          <li className="list-group-item" key={items} onClick={handleClick}>
+        {items.map((items, index) => (
+          <li
+            className={
+              selectIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {
+              SetIndex(index);
+              onSelectItem(items);
+            }}
+          >
             {items}
           </li>
         ))}
