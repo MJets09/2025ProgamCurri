f85c4bb1b9f38b05a552b0e38364e0d53c24e3c9
MJets09 <mthaoqk@gmail.com>
2025-03-12 20:29:48 -0500

    3.12.25

--- react/reactMosh/src/App.tsx
+++ react/reactMosh/src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import ListGroup from "./components/listGroup";
 import LikeButton from "./components/likeButton";
 import Array from "./components/Array";
+import { produce } from "immer";
 
 let Wu = ["Sun Ce", "Sun Jian"];
 let Shu = ["Liu Bei", "Guan Yu", "Zhang Fei"];
@@ -26,12 +27,43 @@ function App() {
   };
 
   const [city, setCity] = useState({
-    city: 'Minneapolis',
-    state: 'MN'
-  })
+    city: "Minneapolis",
+    state: "MN",
+  });
 
   let updateCity = () => {
-    setCity({...city, city: 'Saint Paul'})
+    setCity({ ...city, city: "Saint Paul" });
+  };
+
+  const [shoe, setShoe] = useState([{
+    model: 'Jordan 1',
+    make: 'Nike',
+    have: 'nope'
+  },
+  {
+    model: '350',
+    make: 'Yzy',
+    have: 'nope'
+  }])
+
+
+  const updateShoe = () => {
+    // setShoe(shoe.map(shoe => shoe.model == 'Jordan 1' ? {...shoe, have: true} : shoe))
+    // console.log(shoe)
+
+    //Using the library Immer to simplify our map method above
+
+
+    //Think of the draft as a copy of the array you are using, mutating the object instead of creating a new array
+    setShoe(produce((draft)=>{
+      //variable of shoe is holding/referencing the object you had searched for
+      const shoe = draft.find(shoe => shoe.model == 'Jordan 1');
+      if(shoe){
+        shoe.have = 'yep';
+      }
+    }))
+
+
   }
 
   return (
@@ -42,13 +74,15 @@ function App() {
       <Button onClick={() => updateDrink()}>
         Your drink is {drink.drinkName}
       </Button>
-      <Button onClick={()=> updateCity()}>You live in {city.city}</Button>
+      <Button onClick={()=> updateShoe()}>Jordan 1</Button>
+      <Button onClick={() => updateCity()}>You live in {city.city}</Button>
       <Array array={Shu}></Array>
       <ListGroup items={Wu} heading="Wu"></ListGroup>
       <LikeButton></LikeButton>
+
+      {shoe.map(shoe => <p>{shoe.model}: {shoe.have} </p>)}
     </>
   );
 }
 
 export default App;
-
