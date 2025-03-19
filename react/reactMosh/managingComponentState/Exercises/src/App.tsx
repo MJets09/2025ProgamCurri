7763073f2016bff2bf5ac11ca3d7120d6677ee15
MJets09 <mthaoqk@gmail.com>
2025-03-18 20:37:43 -0500

    3.18.25

--- react/reactMosh/managingComponentState/Exercises/src/App.tsx
+++ react/reactMosh/managingComponentState/Exercises/src/App.tsx
@@ -1,40 +1,64 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [game, setGame] = useState({
-    id:1,
+    id: 1,
     player: {
-      name: 'John'
-    }
-  })
+      name: "John",
+    },
+  });
 
   const updateName = () => {
-      let user = prompt('Your name is?')
-      if(user){
-        setGame({...game, player: {...game.player, name: user}})
-      }
-      else {
-        alert('Enter a name')
-        updateName()
-      }
-      
-  }
+    let user = prompt("Your name is?");
+    if (user) {
+      setGame({ ...game, player: { ...game.player, name: user } });
+    } else {
+      alert("Enter a name");
+      updateName();
+    }
+  };
 
-
-  const [pizza, addTopping] = useState(['Pepproni'])
+  const [pizza, addTopping] = useState(["Pepproni"]);
 
   const addTopp = () => {
-    let user = prompt('What toppings would you like?')
+    let user = prompt("What toppings would you like?");
 
-      if(user){
-        addTopping([...pizza, user])
-      }
-      else {
-        alert('Enter a name')
-        updateName()
-      }
-  }
+    if (user) {
+      addTopping([...pizza, user]);
+    } else {
+      alert("Enter a name");
+      updateName();
+    }
+  };
+
+  const [cart, setCart] = useState({
+    discount: 1,
+    items: [
+      {
+        id: 1,
+        title: "Product1",
+        quantity: 1,
+      },
+      {
+        id: 2,
+        title: "Product2",
+        quantity: 1,
+      },
+    ],
+  });
+
+  // const updateCart = () => {
+  //   setCart({...cart,  items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity +1} : item)})
+  // }
+
+    const updateCart = () => {
+
+      setCart({...cart, items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity+1} : item)})
+
+
+    }
+
 
   return (
     <>
@@ -43,11 +67,15 @@ function App() {
       <button onClick={updateName}>Change Name</button>
       <p>Here is what we have for a current toppings</p>
       <ul>
-      {pizza.map(toppings => <li key={toppings}>{toppings}</li>)}
+        {pizza.map((toppings) => (
+          <li key={toppings}>{toppings}</li>
+        ))}
       </ul>
       <button onClick={addTopp}>Add a topping</button>
+      <button onClick={updateCart}>Button</button>
+      {cart.items[0].quantity}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
