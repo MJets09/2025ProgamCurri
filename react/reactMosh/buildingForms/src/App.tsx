6ef371daba2e360594365ece02e4e4939c7b82ff
MJets09 <mthaoqk@gmail.com>
2025-03-24 20:45:28 -0500

    Handling forms

---
+++ react/reactMosh/buildingForms/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './assets/components/form'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Building forms</h1>
+    <Form></Form>
+    </>
+  )
+}
+
+export default App
