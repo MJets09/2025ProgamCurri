b369ef705c56cbad96257017c9878a004c77847a
MJets09 <mthaoqk@gmail.com>
2025-03-25 21:25:32 -0500

    3.25.25

--- react/reactMosh/buildingForms/src/App.tsx
+++ react/reactMosh/buildingForms/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Form from './assets/components/form'
+import Form from './assets/components/Form'
 
 function App() {
   
--- react/reactMosh/buildingForms/src/assets/components/Form.tsx
+++ react/reactMosh/buildingForms/src/assets/components/Form.tsx
@@ -1,27 +1,41 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
+import {FieldValues, useForm} from 'react-hook-form'
 
 const Form = () => {
 
-    const handleClick = (event: FormEvent) => {
-        event.preventDefault();
-        console.log('Submitted')
-    }
+  const {register, handleSubmit} = useForm()
+  const onSubmit = (data: FieldValues) => console.log(data)
 
+ 
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const ageRef = useRef<HTMLInputElement>(null);
+  // const person = {name: '', age: 0};
+
+  const handleClick = (event: FormEvent) => {
+    // event.preventDefault();
+    // if(nameRef.current !== null)
+    //   person.name = nameRef.current.value;
+    // if(ageRef.current !== null)
+    //   person.age = parseInt(ageRef.current.value);
+
+    // console.log(person)
+
+  };
 
   return (
-    <form onSubmit={handleClick}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input {...register('name')} id="name" type="text" className="form-control" />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input  {...register('age')}id="age" type="number" className="form-control" />
       </div>
 
       <button className="btn btn-primary" type="submit">
