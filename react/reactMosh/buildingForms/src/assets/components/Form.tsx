a93d645a861f9a22db6b7b3450c9558fb837621c
Meng Thao <mthaoqk@gmail.com>
2025-03-31 19:35:25 -0500

    3.31.25

--- react/reactMosh/buildingForms/src/assets/components/Form.tsx
+++ react/reactMosh/buildingForms/src/assets/components/Form.tsx
@@ -1,4 +1,3 @@
-import { FormEvent, useRef } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
@@ -8,19 +7,18 @@ const Form = () => {
   // const nameRef = useRef<HTMLInputElement>(null);
   // const ageRef = useRef<HTMLInputElement>(null);
   // const person = {name: '', age: 0};
+  // const handleClick = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   if(nameRef.current !== null)
+  //     person.name = nameRef.current.value;
+  //   if(ageRef.current !== null)
+  //     person.age = parseInt(ageRef.current.value);
+  //   console.log(person)
+  // };
 
-  const {register, handleSubmit} = useForm();
-  
-  const onSubmit = (data: FieldValues) => console.log(data)
+  const { register, handleSubmit, formState: {errors} } = useForm();
 
-  const handleClick = (event: FormEvent) => {
-    // event.preventDefault();
-    // if(nameRef.current !== null)
-    //   person.name = nameRef.current.value;
-    // if(ageRef.current !== null)
-    //   person.age = parseInt(ageRef.current.value);
-    // console.log(person)
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -28,14 +26,26 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input {...register('name')}id="name" type="text" className="form-control" />
+        <input
+          {...register("name", {required: true, minLength: 2})}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === 'required' && <p>Name field is required</p>}
+        {errors.name?.type === 'minLength' && <p>Minumum length is 2</p>}
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input {...register('age')}id="age" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
 
       <button className="btn btn-primary" type="submit">
