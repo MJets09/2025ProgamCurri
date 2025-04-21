bf9df2109a2dc7abf69ef8e9ddbb7e88bd8d32d1
Meng <mthaoqk@gmail.com>
2025-04-21 18:54:57 -0500

    4.21.25

--- react/reactMosh/managingComponentState/practices/src/App.tsx
+++ react/reactMosh/managingComponentState/practices/src/App.tsx
@@ -8,14 +8,21 @@ function App() {
 
   const Wu = ["Cao Cao", "Liu Bei", "Sun Jian"];
   const Movies = ['There\'s something about Mary'];
+  const NBA = ['Wolves','Knicks','Rockets']
 
   const [close, SetClose] = useState(false);
   const [list, addList] = useState<string[]>([]);
+
   const [inputValue, setInputValue] = useState('');
   const [inputValueWu, setInputValueWu] = useState('');
+
   const [wList, SetW] = useState<string[]>(Wu);
   const [movie, setMovie] = useState<string[]>(Movies);
   const [addedMovie, addMovie] = useState('');
+
+  const [nbaTeam, setTeam] = useState('');
+  const [nbaList, setNBA] = useState<string[]>(NBA)
+
 
   const handleClick = () => {
       addList((prevList) => [...prevList, inputValue]);
@@ -47,12 +54,24 @@ function App() {
   }
 
 
+  const addNBA = () => {
+    console.log(nbaTeam);
+    setNBA((prevList) => [...prevList,nbaTeam]);
+
+  }
+
+  const deleteNBA = (item:string) => {
+
+    setNBA((prevList) => prevList.filter((i) => i !== item))
+    setTeam('')
+  }
+
   return (
     <>
       <h1>Practices</h1>
       <div>
         <ListGroup arr={wList} onDelete={handleWu}></ListGroup>
-        <ShowHide onClose={() => SetClose(!close)}>Click</ShowHide>
+        <ShowHide onClose={() => SetClose(!close)}>Show me</ShowHide>
         {close && <button onClick={() => SetClose(false)}>Sup</button>}
         <input type="text" value={inputValueWu} onChange={(e) => setInputValueWu(e.target.value)}></input>
         <Button onClick={handleClickW}>Add</Button>
@@ -68,12 +87,17 @@ function App() {
       </div>
 
 
-
       <div>
 
       <ListGroup arr={movie} onDelete={deleteMovie}></ListGroup>
       <input type="text" value={addedMovie} onChange={(e) => addMovie(e.target.value)}/>
       <button onClick={handleMovie}>Gimme</button>
+      </div>
+
+      <div>
+        <ListGroup arr={nbaList} onDelete={deleteNBA}></ListGroup>
+        <input type="text" value={nbaTeam} onChange={(e)=> {setTeam(e.target.value)}}/>
+        <button onClick={addNBA}>Add team</button>
       </div>
     </>
   );
