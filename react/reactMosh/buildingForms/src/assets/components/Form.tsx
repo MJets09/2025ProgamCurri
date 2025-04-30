210946a612f14e2a2edd4c7b7e23577acb46b6e2
Meng Thao <mthaoqk@gmail.com>
2025-04-29 21:46:03 -0500

    4.29.25

--- react/reactMosh/buildingForms/src/assets/components/Form.tsx
+++ react/reactMosh/buildingForms/src/assets/components/Form.tsx
@@ -1,70 +1,62 @@
+import React, { FormEvent, useRef } from "react";
 import { FieldValues, useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 const Form = () => {
-  // const {register, handleSubmit} = useForm()
-  // const onSubmit = (data: FieldValues) => console.log(data)
+  const { register, handleSubmit } = useForm();
+  console.log(register("name"));
+
+  const onSubmit = (data: FieldValues) => console.log(data)
 
   // const nameRef = useRef<HTMLInputElement>(null);
   // const ageRef = useRef<HTMLInputElement>(null);
-  // const person = {name: '', age: 0};
-  // const handleClick = (event: FormEvent) => {
-  //   event.preventDefault();
-  //   if(nameRef.current !== null)
-  //     person.name = nameRef.current.value;
-  //   if(ageRef.current !== null)
-  //     person.age = parseInt(ageRef.current.value);
-  //   console.log(person)
+
+  // const person = {
+  //   name: "",
+  //   age: 0,
   // };
 
-  const schema = z.object({
-    name: z.string().min(3),
-    age: z.number({invalid_type_error: 'Age is required'}).min(21),
-  });
+  //TS doesnt know the type of parameter, refer to the onSubmit parameter
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   if (nameRef.current !== null) {
+  //     person.name = nameRef.current.value;
+  //   }
+  //   if (ageRef.current !== null) {
+  //     person.age = parseInt(ageRef.current.value);
+  //   }
+  //   console.log(person);
+  // };
 
-  type FormData = z.infer<typeof schema>
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm<FormData>({resolver: zodResolver(schema)});
-
-  const onSubmit = (data: FieldValues) => console.log(data);
+  //With react hook-forms we can use the register object from useForm() to do what we did with the handleSubmit function;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">
-          Name
-        </label>
-        <input
-          {...register("name",)}
-          id="name"
-          type="text"
-          className="form-control"
-        />
-        {errors.name && <p>{errors.name.message}</p>}
-      </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="userName">Username: </label>
+          <input
+            {...register("name")}
+            // ref={nameRef} Removed the useRef because of react hook forms
+            type="text"
+            id="userName"
+            className="form-control"
+          />
+        </div>
 
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
-        </label>
-        <input
-          {...register("age", {valueAsNumber: true})}
-          id="age"
-          type="number"
-          className="form-control"
-        />
-        {errors.age && <p>{errors.age.message}</p>}
-      </div>
+        <div className="mb-3">
+          <label htmlFor="userAge">Age:</label>
+          <input
+            {...register("age")}
+            // ref={ageRef} Removed the useRef because of react hook forms
+            type="number"
+            id="userAge"
+            className="form-control"
+          />
+        </div>
 
-      <button disabled={!isValid} className="btn btn-primary" type="submit">
-        Submit
-      </button>
-    </form>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </>
   );
 };
 
