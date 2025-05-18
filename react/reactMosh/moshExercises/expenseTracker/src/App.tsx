b645b95491814207bfd439a10b57488350759f2b
Meng <mthaoqk@gmail.com>
2025-05-18 16:59:13 -0500

    5.18.25

--- react/reactMosh/moshExercises/expenseTracker/src/App.tsx
+++ react/reactMosh/moshExercises/expenseTracker/src/App.tsx
@@ -3,20 +3,26 @@ import "./App.css";
 import ExpenseList from "./assets/components/ExpenseList";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 1,
       description: "Shoes",
       amount: 180,
       category: "Bullshit",
     },
-  ];
+    {
+      id: 2,
+      description: "Food",
+      amount: 200,
+      category: "Food",
+    },
+  ]);
 
   return (
     <>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log("Working", id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       ></ExpenseList>
     </>
   );
