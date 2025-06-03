d56c1fa1e1f4997f47a492e8700d75c1f49780cd
Meng Thao <mthaoqk@gmail.com>
2025-06-02 21:20:14 -0500

    6.2.25

--- react/reactMosh_TypeScript/managingComponentState/src/App.tsx
+++ react/reactMosh_TypeScript/managingComponentState/src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import ListGroup from "./components/listGroup";
-
+import ListGroup from "./components/ListGroup";
 
 function App() {
 
   const thingsToMove = ['Shoes', 'Computer']
+  const weekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+
+  const loggedElement = (element: string) => {
+    element == 'Tuesday' && alert('Its Tuesday')
+    console.log(element)
+  }
 
 
   return (
     <>
-  <ListGroup array={thingsToMove}></ListGroup>
+  <ListGroup array={thingsToMove} heading={'Moving Out'} selectedItem={loggedElement}></ListGroup>
+  <ListGroup array={weekDays} heading={'Weekdays'} selectedItem={loggedElement}></ListGroup>
     </>
   );
 }
--- react/reactMosh_TypeScript/managingComponentState/src/components/listGroup.tsx
+++ react/reactMosh_TypeScript/managingComponentState/src/components/listGroup.tsx
@@ -2,21 +2,29 @@ import React, { useState } from "react";
 
 interface Props {
   array: string[];
+  heading: string;
+  selectedItem?: (element: string) => void;
 }
 
-const ListGroup = ({ array }: Props) => {
+const ListGroup = ({ array, heading, selectedItem}: Props) => {
   const [active, setActive] = useState<number | null>(null);
 
   return (
     <>
       {array.length === 0 && <p>Nothing to see</p>}
+      <h1 className="container d-flex justify-content-center mt-5">
+        {heading}
+      </h1>
       <div className="container d-flex justify-content-center mt-5">
         <ul className="list-group">
-          {array.map((element, id) => (
+          {array.map((element, index) => (
             <li
-              key={id}
-              className={`list-group-item ${active === id ? "active" : ""}`}
-              onClick={() => setActive(id)}
+              key={index}
+              className={`list-group-item ${active === index ? "active" : ""}`}
+              onClick={() => {
+                setActive(index);
+                selectedItem && selectedItem(element)
+              }}
             >
               {element}
             </li>
